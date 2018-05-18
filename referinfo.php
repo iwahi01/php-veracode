7bcf781d6c258541973f2c0d97d3fad937a11af6
iwahi01 <17076819+iwahi01@users.noreply.github.com>
2018-05-18 22:56:35 +0900

    イテレーション2

--- referinfo.php
+++ referinfo.php
@@ -1,6 +1,6 @@
 <?php
 //??
-echo "<html>";
+echo "<html> ";
 echo '<head>';
 echo '<meta http-equiv="Content-Type" content="text/html; charset="utf-8" />';
 echo '<link rel="stylesheet" type="text/css" href="gonomiya.css"  />';
