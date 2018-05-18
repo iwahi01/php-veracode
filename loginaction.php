c842e54a83bbdd1c016946ad0a7898f9b0ffd201
iwahi01 <17076819+iwahi01@users.noreply.github.com>
2018-05-19 05:30:47 +0900

    連続修正

--- loginaction.php
+++ loginaction.php
@@ -1,5 +1,5 @@
 <?php
-echo '<html>';
+echo '<html> ';
 echo '<head>';
 echo '<meta http-equiv="Content-Type" content="text/html; charset="utf-8" />';
 echo '<link rel="stylesheet" type="text/css" href="gonomiya.css"  />';
